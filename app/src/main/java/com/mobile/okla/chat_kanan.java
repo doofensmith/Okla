7330f916158eb00cc99e7ab1e2c510c3b3095909
tanpa <alsigitguntoro@gmail.com>
2021-02-03 09:55:33 +0700

    chat box display

---
+++ app/src/main/java/com/mobile/okla/chat_kanan.java
@@ -0,0 +1,14 @@
+package com.mobile.okla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chat_kanan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_kanan);
+    }
+}
